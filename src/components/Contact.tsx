47d52c3d9ed7c93860f8192d73313c34612eb4ba
Syed Ammar Ahmed <ammar.ahmed@hysabkytab.com>
2024-03-05 02:06:47 +0500

    changes for testing git action

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -148,7 +148,7 @@ ${name}${lastUpdatedField === "name" ? (cursorBlink ? "|" : " ") : ""}
   // // 🚀 Initiating Quantum Email Transmission 🪐
   // const launchEmail = async () => {
   //   try {
-  //     const response = await fetch('https://alpaycelik.dev/send',{
+  //     const response = await fetch('https://ammar-ahmed.dev/send',{
   //     method: 'POST',
   //     headers: {'Content-Type': 'application/json'},
   //     body: JSON.stringify({
